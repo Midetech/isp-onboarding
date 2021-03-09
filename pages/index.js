f23ba83ec9adb74144835cf861a2c1a1f9219830
Midecodes <faginorish@gmail.com>
2021-03-09 22:45:01 +0100

    Consume create partner endpoint

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,114 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  let errMessage = '';
+  const [state, setState] = useState('');
+
+const handleChange = e =>{
+  setState(e.target.value)
+}
+
+  const registerUser = async (event) => {
+    event.preventDefault();
+    const res = await fetch(
+      "https://staging.api.admin.plentywaka.com/api/v1/isp/partners/create",
+      {
+        body: JSON.stringify({
+          company_name: event.target.companyNname.value,
+          company_email: event.target.email.value,
+          company_phone: event.target.phone.value,
+          residential_address: event.target.address.value,
+          has_existing_system: state,
+          name: event.target.name.value,
+          password: event.target.password.value,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      }
+      
+    );
+
+    const result = await res.json();
+    // result.user => 'Ada Lovelace'
+      errMessage = result.message;
+     alert(errMessage);
+    
+  };
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+     <p>{ errMessage }</p>
+      <form onSubmit={registerUser}>
+        <label htmlFor="company_name">Name</label>
+        <input id="companyNname" name="companyNname" type="text" autoComplete="companyNname"  />
+        <br />
+        <label htmlFor="company_email">Email</label>
+        <input
+          id="email"
+          name="email"
+          type="text"
+          autoComplete="email"
+          
+        />
+        <br />
+        <label htmlFor="company_phone">phone</label>
+        <input
+          id="phone"
+          name="phone"
+          type="text"
+          autoComplete="phone"
+          
+        />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <br />
+        <label htmlFor="residential_address">Address</label>
+        <input
+          id="address"
+          name="address"
+          type="text"
+          autoComplete="address"
+          
+        />
+         <br />
+        <label htmlFor="name">Admin Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          autoComplete="name"
+          
+        />
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+        <br />
+        <label htmlFor="has_existing_system">Has Existing System</label>
+        <select
+        onChange={handleChange}
+        value={state}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+          <option disabled selected>Select an option</option>
+           <option value='1'>Yes</option>
+            <option value='0'>No</option>
+        </select>
+
+<br />
+        <label htmlFor="password">Passsword</label>
+        <input
+          id="password"
+          name="password"
+          type="text"
+          autoComplete="password"
+          
+        />
+
+       <br/>
+
+        <button type="submit">Register</button>
+      </form>
     </div>
-  )
+  );
 }
